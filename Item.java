db7da8159230ffcd8b29c7e75f4f22979fb4585c
jmarz99 <62476627+jmarz99@users.noreply.github.com>
2020-04-12 03:51:39 -1200

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,37 @@
+class Item {
+	
+int id;
+	String name;
+	
+double price;
+
+
+int a=18%;
+int b=5%;
+int c=0%;
+int naocari = 500;
+int corapi = 200;
+int maica = 700;
+
+	//TODO add variable.
+
+	//TODO constructor
+
+	//TODO setters and getters
+
+	
+double taxReturn () {
+		//TODO
+	double getTaxReturn(){
+		return naocari-15%;
+		
+	}
+}
+}
+class Receipt {
+	List<String> al=new ArrayList<String>();  
+	al.add("naocari");  
+	al.add("corapi");  
+	al.add("maica");  
+	al.add(1,"farmerki"); 
+}
